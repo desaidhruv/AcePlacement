376ee08e871476a9d6e66a0abdb2d14fbb97db6e
Dhruv Desai <dhruvshah28@gmail.com>
2021-08-19 13:10:04 +0530

    Requirements Added

--- TPO_website/TPO_website/settings.py
+++ TPO_website/TPO_website/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'bts9@r!(gyy0lg(ut@_zh98wmgalt9s^0sif)0%q_u%v^ga4)x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tpo-web.herokuapp.com', '127.0.0.1:8000']
 
 
 # Application definition
